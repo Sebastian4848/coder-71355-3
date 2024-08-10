629f4ab227bcb980ecfcd98cc985ba003a427f92
Sebastian Ojeda <sebastian.ojeda.msc@gmail.com>
2024-08-10 17:58:04 -0400

    update

--- js/main.js
+++ js/main.js
@@ -58,58 +58,32 @@ class Sistema {
     }
 
     //? Opcion 2: Agregar nuevo herraje al inventario, Se declara como un metodo dentro de la la clase contructora.
-
     nuevoHerraje() {
-
         let nombre = document.getElementById("herraje1").value;
         let stock = document.getElementById("stock1").value;
         this.herrajes.push(new Herraje(nombre, stock))
-        // mostrarTabla(herrajes);
+        mostrarTabla(herrajes);
         actualizarTabla(herrajes)
-
-        // const nombre = solicitarDato('cadena', 'Ingrese el nombre del herraje', 'Herraje Dorado 2.5cm');
-        // const stock = solicitarDato('entero', 'Ingrese el stock actual del herraje', '10');
-        // this.herrajes.push(new Herraje(nombre, stock))
-        // mostrarTabla(herrajes);
-        // actualizarTabla(herrajes)
     }
 }
-
-
 
 
 //? SISTEMA: Definicion de las funciones principales del sistema.
 let sistema_stock = new Sistema(herrajes)
 
-//? Menu principal.
 
-// const menu_str = `
-//     Presiona 1 para mostrar el stock actual de herrajes.
-//     Presiona 2 para agregar un nuevo herraje al inventario.
-//     Presiona 3 para buscar herrajes.
-//     Presiona 4 para agregar una incidencia al stock.
-//     Presiona 5 para mostrar stock bajo.
-//     Presiona 6 para eliminar un herraje.
-//     Presiona 7 para reasignar los IDs de los herrajes.
-//     Presiona 0 para VOLVER o SALIR.
-// `
-
-
-
-//? Opcion 1: Mostrar tabla.
+// //? Opcion 1: Mostrar tabla rn la consola para usar en el debug
 function mostrarTabla(datos) {
     // console.clear();
     console.table(datos);
 }
+mostrarTabla(herrajes)
 
 //? Opcion 3: Buscar herrajes.
 function filtrarHerrajes(herraje) {
-
-    // let busqueda = solicitarDato('cadena_minusculas', 'Ingrese el nombre o alguna caracteristica del herraje', 'Argolla');
     let busqueda = document.getElementById("busqueda1").value;
-    // const result = herrajes.filter((herraje) => herraje.nombre.toLowerCase().indexOf(busqueda) != -1);
     const result = herrajes.filter((herraje) => herraje.nombre.toLowerCase().includes(busqueda.toLocaleLowerCase()));
-    // mostrarTabla(result);
+    mostrarTabla(result);
     actualizarTabla(result)
     if (result.length == 0) {
         // alert(`La busqueda de ${busqueda} arrojo 0 resultados`);
@@ -122,8 +96,6 @@ function filtrarHerrajes(herraje) {
 function agregarIncidencia(herrajes) {
     let id_herraje = document.getElementById("id1").value;
     let cantidad = parseInt(document.getElementById("stock2").value, 10);
-    // let id_herraje = solicitarDato('entero_positivo', 'Ingrese el ID del herraje', '1');
-    // let cantidad = solicitarDato('entero', 'Ingrese la cantidad de de herrajes a sumar o restar', '5');
     herrajes[id_herraje - 1].stock = herrajes[id_herraje - 1].stock + cantidad;
     mostrarTabla(herrajes);
     actualizarTabla(herrajes)
@@ -131,7 +103,6 @@ function agregarIncidencia(herrajes) {
 
 //? Opcion 5: Mostrar stock bajo
 function stockBajo(herrajes) {
-
     let busqueda = parseInt(document.getElementById("stock_bajo1").value, 10);
     console.log(busqueda)
     const result = herrajes.filter((herraje) => herraje.stock <= busqueda);
@@ -146,15 +117,21 @@ function stockBajo(herrajes) {
 
 //? Opcion 6: Eliminar herraje
 function eliminarHerraje(herrajes) {
-
     let id_herraje = parseInt(document.getElementById("id2").value, 10);
-
-    // let id_herraje = solicitarDato('entero_positivo', 'Ingrese el ID del herraje', '1');
+    console.log(id_herraje)
     const herrajeID = herrajes.find((herraje) => herraje.id === id_herraje)
+    console.log(herrajeID)
     const index = herrajes.indexOf(herrajeID);
+    console.log(index)
+if (index === -1) {
+    console.log(`No se encontro el herraje con ID ${id_herraje}`);
+    return;
+}else{
     herrajes.splice([index], 1);
     mostrarTabla(herrajes);
     actualizarTabla(herrajes)
+}
+
 }
 
 //? Opcion 7: Regenerar IDs
@@ -162,35 +139,38 @@ function idReset(herrajes) {
     for (let i = 0; i < herrajes.length; i++) {
         herrajes[i].id = i + 1;
     }
-    mostrarTabla(herrajes);
+    // mostrarTabla(herrajes);
     actualizarTabla(herrajes)
 }
 
-//? Opcion 8: Resetear inventario
-function inventarioReset(herrajes) {
+//? Opcion 8: Regresar al inventario original inicial
+function inventarioOriginal(herrajes) {
     // Hacer una copia profunda del inventario original y reasignarlo
     herrajes.length = 0;  // Vacía el array actual
     for (let herraje of herrajes_og) {
         herrajes.push(new Herraje(herraje.nombre, herraje.stock));  // Reagregar cada herraje del original
     }
-
     actualizarTabla(herrajes);
 }
 
+//? Opcion 9: Resetear inventario
+
+function inventarioReset(herrajes) {
+actualizarTabla(herrajes)
+}
+
+//? Opcion 10: Actualizar tabla en el HTML usando el DOM
 function actualizarTabla(array) {
     let html = html_base;
     for (let i = 0; i < array.length; i++) {
-
         html += '<tr>';
         html += '<td>' + array[i].id + '</td>';
         html += '<td>' + array[i].nombre + '</td>';
         html += '<td>' + array[i].stock + '</td>';
         html += '</tr>';
     }
-
     document.getElementById("table").innerHTML = html
 }
-
 
 actualizarTabla(herrajes)
 
@@ -207,7 +187,9 @@ boton_modificar_stock.addEventListener('click', () => { agregarIncidencia(herraj
 boton_stock_bajo.addEventListener('click', () => { stockBajo(herrajes) })
 boton_eliminar.addEventListener('click', () => { eliminarHerraje(herrajes) })
 boton_id_reset.addEventListener('click', () => { idReset(herrajes) })
-boton_reset.addEventListener('click', () => { inventarioReset(herrajes) })
+// boton_reset.addEventListener('click', () => { inventarioOriginal(herrajes) })
+boton_resetear_busqueda.addEventListener('click', () => { inventarioReset(herrajes) })
+boton_resetear_filtro.addEventListener('click', () => { inventarioReset(herrajes) })
 
 
 
@@ -217,26 +199,6 @@ boton_reset.addEventListener('click', () => { inventarioReset(herrajes) })
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//? MAIN: Interaccion del usuario con el codigo.
-
-// let sistema_stock = new Sistema(herrajes)
 
 
 //? Funciones para hacer validacion de datos de entrada.
@@ -263,43 +225,5 @@ function solicitarDato(tipo, mensaje, sugerencia = '') {
             dato = prompt(mensaje, sugerencia).toLocaleLowerCase();
         } while (dato == '');
     }
-
     return dato;
 }
-
-//? Bucle principal del programa
-
-// let salir = false
-// while (!salir) {
-
-//     let opcion = parseInt(prompt(menu_str));
-//     switch (opcion) {
-//         case 0:
-//             alert("FIN. Para volver al menu principal presiona F5")
-//             salir = true
-//             break
-//         case 1:
-//             mostrarTabla(herrajes);
-//             break
-//         case 2:
-//             sistema_stock.nuevoHerraje();
-//             break
-//         case 3:
-//             filtrarHerrajes(herrajes);
-//             break
-//         case 4:
-//             agregarIncidencia(herrajes);
-//             break
-//         case 5:
-//             stockBajo(herrajes);
-//             break
-//         case 6:
-//             eliminarHerraje(herrajes);
-//             break
-//         case 7:
-//             idReset(herrajes);
-//             break
-//         default:
-//             alert("Opcion inexistente. Intente de nuevo");
-//     }
-// }
